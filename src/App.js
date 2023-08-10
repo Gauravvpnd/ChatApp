c27ad144008d144cd0e9c45c9f7341d23a147b67
Gauravvpnd <gouravpanda2k04@gmail.com~>
2023-08-10 18:59:31 +0530

    Need to fix some problems in App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react'
+import { Button, InputLabel  } from '@mui/material'
+import Message from './Message'
+const App = () => {
 
-function App() {
+  const [Input, setInput] = useState('') 
+  const [message, setMessage] = useState(
+     {userName: 'Gaurav', text: 'Heyy'},
+    {userName: 'Nikk', text: 'Heyy you'}
+    
+  )
+   
+  
+  const [userName, setUsername] = useState('')
+
+  useEffect(() => {
+    setUsername(prompt('Enter your name'))
+  },[])
+ 
+  console.log(message)
+  const sendMessage = (e) => {
+    e.preventDefault()
+    setMessage([...message,{userName: userName, text: Input}])
+    setInput('')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+       <div className="app">
+      <h1>Hello Welcome to ChatOin</h1>
+      <h2>Welcome {userName}</h2>
+      
+      <form>
+         <InputLabel>Enter your message....</InputLabel>
+          <input value={Input} onChange={e => setInput(e.target.value)} />
+       <Button disabled={!Input} type='submit' onClick={sendMessage}>Send</Button>
+      </form>
+    {
+        message.map(message => (
+         <Message  userName={message.userName} text={message} />
+        ))
+    }
     </div>
-  );
+   
+   
+  )
 }
 
-export default App;
+export default App
