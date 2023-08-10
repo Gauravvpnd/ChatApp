5abe46d0751d6aab1c834c809382820a783d8d7f
Gauravvpnd <gouravpanda2k04@gmail.com~>
2023-08-10 19:05:55 +0530

    changes in meesage

--- src/Message.js
+++ src/Message.js
@@ -1,9 +1,16 @@
+import { Card, CardContent, Typography } from '@mui/material'
 import React from 'react'
 
 const Message = (props) => {
   return (
     <div>
-        <h2>{props.userName}: {props.text}</h2>
+      <Card>
+        <CardContent>
+          <Typography color="white" variant='h5' component='h2'>
+          {props.userName}: {props.text}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   )
 }
